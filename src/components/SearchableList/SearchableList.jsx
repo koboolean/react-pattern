dd44aed71ef1e1297a7cae68f36ce317f1320e0f
조현준 <45157196+koboolean@users.noreply.github.com>
2024-11-19 20:19:28 +0900

    Debouncing

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,17 +1,27 @@
-import {useState} from "react";
+import {useCallback, useRef, useState} from "react";
 
 export default function SearchableList({items, itemKeyFn, children}){
 
     const [searchTerm, setSearchTerm] = useState('');
 
+    const lastChange = useRef();
+
     const searchResult = items.filter(item => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()));
 
-    const handleChange = (evnet) => {
-        setSearchTerm(event.target.value);
+    const handleChange = (event) => {
+
+        if(lastChange.current){
+            clearTimeout(lastChange.current);
+        }
+
+        lastChange.current = setTimeout(() => {
+            lastChange.current = null;
+            setSearchTerm(event.target.value);
+        }, 500);
     }
 
     return <div className={"searchable-list"}>
-        <input type={"search"} placeholder={"Search"} onChange={handleChange}/>
+        <input type={"search"} placeholder={"Search"} onChange={(event) => handleChange(event)}/>
         <ul>
             {searchResult.map((item, index) => (
                 <li key={itemKeyFn(item)}>
